05099dc5eb476f796bf036a1f11265107e2f19c0
Apolo-at-403082213880 <apolo.ferreira@datarain.com.br>
2025-09-10 08:12:38 -0300

    remove

--- doc/exceptions.py
+++
@@ -1,8 +0,0 @@
-
-class RecordNotFoundError(Exception):
-    """Exception raised when a record does not exist in the table."""
-    pass
-
-class PrimaryKeyDuplicatedError(Exception):
-    """Exception raised when a record is duplicated in the table."""
-    pass
